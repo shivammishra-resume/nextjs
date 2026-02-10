34817c5208efecebafeb840fa95d27c36774bed4
Shivam <shivammishra.resume@gmail.com>
2026-02-10 21:39:39 +0530

    Dynamic Routes and Nested Dynamic Routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function products(){
+    return <div>
+        <h1>This is Product page.</h1>
+        <h2>Product 1</h2>
+        <h2>Product 2</h2>
+        <h2>Product 3</h2>
+    </div>
+}
