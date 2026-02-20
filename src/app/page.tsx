a7946f2d775b7ed0c083ca21fa076e73e1a75020
Shivam <shivammishra.resume@gmail.com>
2026-02-20 16:37:10 +0530

    params and searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home(){
         <h1>Hello world!</h1>
         <Link href="/blog">Blog</Link>
         <Link href="/products">Products</Link>
+        <Link href="/articles/breaking-news-123?lang=en">Read in english</Link>
+        <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
         </>
     )
 }
