7f92d8d987459e664b0f55457cd8092c683cc9b6
Shivam <shivammishra.resume@gmail.com>
2026-02-20 16:15:25 +0530

    Active Links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link"
+
 export default function products(){
+    const productId = 100;
     return <div>
+        <>
+        <Link href="/">Home</Link>
         <h1>This is Product page.</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2><Link href="/products/1">Product 1</Link></h2>
+        <h2><Link href="/products/2">Product 2</Link></h2>
+        <h2><Link href="/products/3" replace>Product 3</Link></h2>
+        <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
+        </>
     </div>
 }
