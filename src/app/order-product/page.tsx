cbbea7ce40f926496a7c0194a9ef28041fd0bfe7
Shivam <shivammishra.resume@gmail.com>
2026-02-23 20:23:30 +0530

    Navigating Programmatically

---
+++ src/app/order-product/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useRouter } from "next/navigation";
+
+
+export default function OrderProduct(){
+    const router = useRouter();
+    const handleClick = () => {
+        console.log("Placing Your Order");
+        router.push("/");
+        // router.replace("")
+        // router.back("")
+        // router.forward("")
+    };
+    return (
+        <>
+        <h1>Order product</h1>
+        <button onClick={handleClick}>Place Order</button>
+        </>
+    )
+}
